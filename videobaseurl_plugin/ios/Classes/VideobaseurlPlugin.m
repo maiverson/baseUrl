d1aed3c05f90101546d9633ee82ad64a03f03601
maiverson <276715145@qq.com>
2019-09-07 11:48:32 +0800

    first

---
+++ videobaseurl_plugin/ios/Classes/VideobaseurlPlugin.m
@@ -0,0 +1,20 @@
+#import "VideobaseurlPlugin.h"
+
+@implementation VideobaseurlPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"videobaseurl_plugin"
+            binaryMessenger:[registrar messenger]];
+  VideobaseurlPlugin* instance = [[VideobaseurlPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
---
+++ videobaseurl_plugin/lib/videobaseurl_plugin.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+const String BASE_URL = "http://39.98.219.121/";
+
+
+class VideobaseurlPlugin {
+  static const MethodChannel _channel =
+      const MethodChannel('videobaseurl_plugin');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
